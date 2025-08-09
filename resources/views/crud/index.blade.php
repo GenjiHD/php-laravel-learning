fec5797ad44ed4cd14f1affab69de924f49b9ffe
GenjiHD <joaquincastrosarmientosanchez@gmail.com>
2025-08-08 22:37:11 -0700

    fundamentals of MVC with laravel

---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>CRUD</h1> 
+
+   Forma clasica de php para mostrar el contenido de una variable
+   <h3><?php echo $name ?></h3>
+
+   Forma de laravel
+   <h3>{{ $name }}</h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function(){
+//     return "Welcome";
+// });
+
+Route::get('/test', function(){
+    return view('test');
+});
+
+// Route::get('/crud', function(){
+//     return view('crud/index');
+// });
+
+Route::get('/crud', function() {
+    $age = 21;
+    $data = ['name' => 'Joaquin', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crud');
